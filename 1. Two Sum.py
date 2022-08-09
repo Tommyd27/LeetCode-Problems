a1e6e9b2df8fb36b84e61903b7d31550e5c10b6b
Tom JP Laptop <tom@smart-homes.com>
2022-08-09 16:17:50 +0100

    enumerate is slow

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,9 +1,9 @@
-def twoSum(nums, target):
-    def twoSum(self, nums: list[int], target: int) -> List[int]:
-        subtractionDictionary = {}
-        for i, num in enumerate(nums):
-            if num in subtractionDictionary:
-                return subtractionDictionary[num], 
-            subtractionDictionary[target - num] = i
-        
+def twoSum(self, nums: list[int], target: int) -> List[int]:
+    subtractionDictionary = {}
+    i = 0
+    for num in nums:
+        if num in subtractionDictionary:
+            return subtractionDictionary[num], 
+        subtractionDictionary[target - num] = i
+        i += 1
                 
